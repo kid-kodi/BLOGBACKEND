74aaad2e2e50194ce4b54023d2848cede6bcca8b
Dangui ismael Kone <danguiismaelkone@MacBook-Pro-de-Dangui.local>
2022-02-08 20:19:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+import PostPage from "./pages/PostPage";
+import Navigation from "./components/Navigation";
+import NewPostPage from "./pages/NewPostPage";
+import PostDetailPage from "./pages/PostDetailPage";
+import { Auth } from "./pages/auth/Auth";
+import { PrivateRoute } from "./helpers/PrivateRoute";
+import CategoryPage from "./pages/category/CategoryPage";
+import NewCategoryPage from "./pages/category/NewCategoryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <Router>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <div className="absolute top-0 left-0 right-0 bottom-0 overflow-hidden">
+            <div className="absolute top-0 left-0 bottom-0 w-[300px] bg-gray-100 p-4 overflow-auto">
+              <Navigation />
+            </div>
+            <div className="bg-white absolute top-0 left-[300px] right-0 bottom-0 overflow-auto">
+              <PrivateRoute
+                exact
+                path="/posts/detail/:id"
+                component={PostDetailPage}
+              />
+              <PrivateRoute
+                exact
+                path="/posts/edit/:id"
+                component={NewPostPage}
+              />
+              <PrivateRoute exact path="/posts/add" component={NewPostPage} />
+              <PrivateRoute exact path="/posts" component={PostPage} />
+              <PrivateRoute exact path="/categories" component={CategoryPage} />
+              <PrivateRoute
+                exact
+                path="/categories/add"
+                component={NewCategoryPage}
+              />
+              <PrivateRoute
+                exact
+                path="/categories/edit/:id"
+                component={NewCategoryPage}
+              />
+              <PrivateRoute exact path="/" component={PostPage} />
+            </div>
+          </div>
+        </Switch>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
